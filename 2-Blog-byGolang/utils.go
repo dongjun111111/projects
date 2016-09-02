147064694289b8c3818572e35bc1dae8ef7aef54
dongjun111111 <903456967@qq.com>
2016-09-03 00:16:06 +0800

    strings

--- 2-Blog-byGolang/utils.go
+++ 2-Blog-byGolang/utils.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"os"
+	"strings"
 	"time"
 )
 
 func writeMainLogToFile(vals []string, outfile string) error {
-	_, err := os.Open(outfile)
-	if err != nil && os.IsNotExist(err) {
-		_, err := os.Create(outfile)
-		if err != nil {
-			panic(err)
+	today := time.Now().Format("2006-01-02")
+	if strings.Contains(outfile, today) {
+		_, err := os.Open(outfile)
+		if err != nil && os.IsNotExist(err) {
+			_, err := os.Create(outfile)
+			if err != nil {
+				panic(err)
+			}
+
 		}
+	} else {
+		panic("data error!")
 	}
+
 	f, err := os.OpenFile(outfile, os.O_APPEND, 0644)
 	if err != nil {
 		panic(err)
